08db63e5317ad4b0efa80fb9d9808493d52bb332
arshia-jr <75935928+arshiajr-coder@users.noreply.github.com>
2023-12-02 16:16:05 +0330

    Update app.js

--- app/js/app.js
+++ app/js/app.js
@@ -5,15 +5,30 @@ const examsList = [
     seasons: "درس ۶و۷",
     isNew: true,
   },
+  {
+    name: "سلام سید",
+    date: "۱۴۰۲/۳/۱5",
+    seasons: "درس ۶و۷",
+    isNew: true,
+  },
+  {
+    name: "سلامت و بهداشت",
+    date: "۱۴۰۲/۳/۱۱",
+    seasons: "درس ۶و۷",
+    isNew: true,
+  },
 ];
 
 const ul = document.querySelector(".ul-list");
 
 examsList.forEach((exam) => {
-  ul.innerHTML += `<li class="relative flex w-[400px] lg:w-[480px] py-2">
-    <div class="w-[122px] lg:w-[140px]">${exam.name}</div>
-    <div class="w-[100px] lg:w-[140px] flex justify-center ml-2">${exam.date}</div>
-    <div class="w-[140px] lg:w-[180px]">${exam.seasons}</div>
-    <div class="flex justify-center items-center w-6 h-6 rounded-xl text-[8px] ${exam.isNew? "": "hidden"} text-white absolute top-0 left-[-7px] lg:top-auto bg-red-400">${exam.isNew? "جدید": ""}</div>
+  ul.innerHTML += `<li class="relative lg:flex lg:w-[500px] py-4 lg:items-center">
+  <div class="lg:w-[180px] text-center">${exam.name}</div>
+  <div class="flex justify-center mt-3 lg:mt-0">
+      <div class="lg:w-[140px] flex justify-center ml-4">${exam.date}</div>
+      <div class="lg:w-[180px]">${exam.seasons}</div>
+  </div>
+  <div class="flex justify-center items-center w-6 h-6 rounded-xl text-[8px] text-white absolute top-0 left-0 lg:top-auto bg-red-400 ${exam.isNew? "": "hidden"}">${exam.isNew? "جدید": ""}</div>
 </li>`;
+
 });
